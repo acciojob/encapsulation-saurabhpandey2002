8d8fb0d821dc04c1ca54c64c24487a6dd7a7c5e5
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-02-12 13:55:07 +0530

    Some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly r = new RWOnly();
+        r.setName("John");  // error
+        System.out.println(r.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
     
 }
